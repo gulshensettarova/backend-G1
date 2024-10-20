f6a2ab4b7e635fa1041f95e93b7788b12d6a5678
gulshensettarova <gulshensettarova@gmail.com>
2024-10-20 13:13:04 +0400

    implement random class

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,8 @@ public class Main {
             System.out.println("i = " + i);
             System.out.println("Salam");
             System.out.println("Sagol");
+
+            System.out.println("Random classi implement olundu");
         
         }
     }
