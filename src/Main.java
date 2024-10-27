4eaaefa53d184cfcdb41fd0cdcfb84cdbf775f9f
Gulshan Sattarova <90262999+gulshensettarova@users.noreply.github.com>
2024-10-27 11:22:08 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
             System.out.println("Salam");
             System.out.println("Random classi implement olundu");
+            System.out.println("Scanner classi implement olundu");          
     }
 }
